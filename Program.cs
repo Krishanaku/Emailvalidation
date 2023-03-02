3e56152ee371b6806e4471cbb2bc6e3396460951
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-02 15:23:58 +0530

    Add : Email Validation

--- Program.cs
+++ Program.cs
@@ -1,10 +1,22 @@
-﻿namespace EmailValidation
+﻿using System.Text.RegularExpressions;
+
+namespace EmailValidation
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Email Validation :-All Sample validate ");
+            string email = "abc@yahoo.com";
+
+            if (Regex.IsMatch(email, @"^[a-zA-Z0-9@.]+$"))
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
         }
     }
 }
