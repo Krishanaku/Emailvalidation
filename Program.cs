15920d082507c214180d47387e968ea520fb8adc
Krishanaku <krishnakumar.kk650.kk@gmail.com>
2023-03-02 15:22:10 +0530

    Add : Email Validation

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailValidation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Email Validation :-All Sample validate ");
+        }
+    }
+}
